78d31e96f6de4610b159dc6a66a79bb419421307
furqan-hue <furqantasiya@gmail.com>
2025-03-04 08:40:11 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = 33;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+
+// score = "tasiya";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInnumber = Number(score);
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+
+
+
+
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
