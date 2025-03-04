fd88ecfc0e22118d9882a8430d9ddacf9ff41af0
furqan-hue <furqantasiya@gmail.com>
2025-03-04 08:14:43 +0000

    varables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 13423;
+let accountEmail = "furqan@google.com";
+let accountPassword = "1234";
+accountCity = "Godhra";
+let accountState;
+
+// accountId = 34;  not allowed
+accountEmail = "fur@gmail.com"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
