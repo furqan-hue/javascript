88e995eacd217f1801e0422b0451efb77fe20794
furqan-hue <furqantasiya@gmail.com>
2025-03-04 09:31:19 +0000

    summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+// primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+
+const id = Symbol("123");
+console.log(typeof id);
+
+
+
+
+// Reference (Non primitive)
+// Array, Objects, Functions
