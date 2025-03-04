5857d0ab13fa3c715cdf40d3d2657a6a0ffb0d13
furqan-hue <furqantasiya@gmail.com>
2025-03-04 08:56:54 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,7 +22,32 @@ let score = 33;
 // console.log(booleanIsLoggedIn);
 
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+
+
+
+// let str1 = "hello";
+// let str2 = "hitesh";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+console.log(+true);
