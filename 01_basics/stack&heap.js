2cd193659c887a2afe5514583c2cb1456a083d2c
furqan-hue <furqantasiya@gmail.com>
2025-03-04 09:50:14 +0000

    stack&heap

---
+++ 01_basics/stack&heap.js
@@ -0,0 +1,9 @@
+// stack (Primtive), heap (Non-Primitive)
+
+let myYoutubename = "hiteshchoudharycom";
+let anothername = myYoutubename;
+// anothername = "chaiaurcode";
+
+console.log(myYoutubename);
+console.log(anothername);
+
