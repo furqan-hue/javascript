a062c0b8f55dd0daef57b7f424b82140d94a900f
furqan-hue <furqantasiya@gmail.com>
2025-03-05 09:37:26 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "furq";
+const repoCount = 50;
+// console.log(name + repoCount + " value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String("furqan-tas");
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf("q"));
+
+console.log(gameName.substring(0, 4));
+console.log(gameName.slice(0, 4));
+console.log(gameName.replace('-', '_'));
+
+console.log(gameName.includes('rk'));
+console.log(gameName.split("-"));
