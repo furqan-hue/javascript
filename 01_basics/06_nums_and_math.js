4a1d6611b926410f01661d02b4b3576f973b7822
furqan-hue <furqantasiya@gmail.com>
2025-03-05 09:38:00 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,16 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+const toStr = balance.toString();
+// console.log(toStr);
+// console.log(toStr.length);
+
+// console.log(balance.toFixed(2));
+
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(4.5));
