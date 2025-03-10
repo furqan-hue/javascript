45530829909fd9457c55afc0ac909313893c7ac4
furqan-hue <furqantasiya@gmail.com>
2025-03-10 09:23:54 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,24 @@
+const isUserloggedIn = true;
+// if(2 == "2"){
+//     console.log("executed");
+// }
+// if(2 != 3){
+//     console.log("executed");
+// }
+
+
+
+
+const balance = 1000;
+if(balance < 500){
+    console.log("less than 500");
+}
+else if(balance < 650){
+    console.log("less than 650");
+}
+else if(balance < 900){
+    console.log("less than 900");
+}
+else{
+    console.log("less than 1200");
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,25 @@
+// const userEmail = "furqan@gmail.com";
+// if(userEmail){
+//     console.log("Got user email");
+// }
+// else{
+//     console.log("Don't have user email");
+// }
+
+
+
+const userEmail = "";
+if(userEmail){
+    console.log("Got user email");
+}
+else{
+    console.log("Don't have user email");
+}
+
+            // falsy values
+ // false: 0, -0, BigInt 0n, "", null, undefined, NaN;
+
+            
+            // truthy values
+
+// truth: "0", "false", " ", [], {}, function(){}
